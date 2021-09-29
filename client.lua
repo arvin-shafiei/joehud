6917178261b5408a8b685cdec809ebc54dfa3751
JoeStarMama <ddashafieiadjbish2@glow.sch.uk>
2021-09-29 20:14:43 +0100

    v1.05

--- client.lua
+++ client.lua
@@ -5,20 +5,21 @@ local Keys = {
     ["ESC"] = 322, ["F1"] = 288, ["F2"] = 289, ["F3"] = 170, ["F5"] = 166, ["CAPSLOCK"] = 171, ["F6"] = 167, ["F7"] = 168, ["F8"] = 169, ["F9"] = 56, ["F10"] = 57, ["~"] = 243, ["1"] = 157, ["2"] = 158, ["3"] = 160, ["4"] = 164, ["5"] = 165, ["6"] = 159, ["7"] = 161, ["8"] = 162, ["9"] = 163, ["-"] = 84, ["="] = 83, ["BACKSPACE"] = 177, ["TAB"] = 37, ["Q"] = 44, ["W"] = 32, ["E"] = 38, ["R"] = 45, ["T"] = 245, ["Y"] = 246, ["U"] = 303, ["P"] = 199, ["["] = 39, ["]"] = 40, ["ENTER"] = 18, ["CAPS"] = 137, ["A"] = 34, ["S"] = 8, ["D"] = 9, ["F"] = 23, ["G"] = 47, ["H"] = 74, ["K"] = 311, ["L"] = 182, ["LEFTSHIFT"] = 21, ["Z"] = 20, ["X"] = 73, ["C"] = 26, ["V"] = 0, ["B"] = 29, ["N"] = 249, ["M"] = 244, [","] = 82, ["."] = 81, ["LEFTCTRL"] = 36, ["LEFTALT"] = 19, ["SPACE"] = 22, ["RIGHTCTRL"] = 70, ["HOME"] = 213, ["PAGEUP"] = 10, ["PAGEDOWN"] = 11, ["DELETE"] = 178, ["LEFT"] = 174, ["RIGHT"] = 175, ["TOP"] = 27, ["DOWN"] = 173, ["NENTER"] = 201, ["N4"] = 108, ["N5"] = 60, ["N6"] = 107, ["N+"] = 96, ["N-"] = 97, ["N7"] = 117, ["N8"] = 61, ["N9"] = 118
 }
 
-local society
 local mapon = true;
 local speedfps = 125;
 local minimap = RequestScaleformMovie("minimap")
 local hunger
 local thirst
 
-local lastJob = nil
-local PlayerData = nil
-local lastcash = nil
-local lastbank = nil
-local lastdirty = nil
-local lastsociety = nil
+local showweap
+local showUi
 
+local society 
+local lastjob
+local lastcash 
+local lastbank 
+local lastdirty 
+local lastsociety 
 
 SetRadarZoom(1150)
 
@@ -31,17 +32,6 @@ Citizen.CreateThread(function()
     end
     loaded = true
     ESX.PlayerData = ESX.GetPlayerData()
-end)
-
-Citizen.CreateThread(function()
-    while ESX == nil do
-        TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-        Citizen.Wait(10)
-    end
-    Citizen.Wait(500)
-    if PlayerData == nil or PlayerData.job == nil then
-        PlayerData = ESX.GetPlayerData()
-    end
 end)
 
 RegisterNetEvent('esx:playerLoaded')
@@ -60,7 +50,6 @@ RegisterKeyMapping('seatbelt', 'Seatbelt', 'keyboard', 'B')
 RegisterCommand('seatbelt', function()
     seatbelt()
 end, false)    
-
 
 local speedBuffer  = {}
 local velBuffer    = {}
@@ -188,7 +177,6 @@ Citizen.CreateThread(function()
             hideseatbelt = true
             showlimiter = true
             showSpeedo = true
-            print(mapon)
             if mapon then
                 ToggleRadar(true)
                 mapoutline = true
@@ -220,10 +208,6 @@ Citizen.CreateThread(function()
                 mapoutline = mapoutline})
         end
         
-        local showweap
-        local showUi
-        local Underwater
-
         if IsEntityInWater(PlayerPedId()) then
             Underwater = true
         else 
@@ -255,11 +239,11 @@ RegisterNetEvent('joehud:setInfo')
 AddEventHandler('joehud:setInfo', function(info)
 
 
-    if PlayerData.job ~= nil then
-        if PlayerData.job.label ~= nil and PlayerData.job.grade_label ~= nil then
+    if ESX.PlayerData.job ~= nil then
+        if ESX.PlayerData.job.grade_name ~= nil and ESX.PlayerData.job.grade_name == 'boss' then
             ESX.TriggerServerCallback('esx_society:getSocietyMoney', function(money)
             society = money
-            end, PlayerData.job.name)
+            end, ESX.PlayerData.job.name)
         else
             society =  0
         end
@@ -267,21 +251,17 @@ AddEventHandler('joehud:setInfo', function(info)
                    
     local player = PlayerPedId()
 
-    local radioStatus = exports["rp-radio"]:IsRadioOn()
+   local radioStatus = exports["rp-radio"]:IsRadioOn()
 
     TriggerEvent('esx_status:getStatus', 'hunger', function(status) hunger = status.val / 10000 end)
 
     TriggerEvent('esx_status:getStatus', 'thirst', function(status) thirst = status.val / 10000 end)
 
-
     SendNUIMessage({radio = radioStatus})
 
-        if(PlayerData ~= nil) and (PlayerData.job ~= nil) then
-            jobName = PlayerData.job.label..' - '..PlayerData.job.grade_label
-            if(lastJob ~= jobName) then
-                lastJob = jobName
-                SendNUIMessage({job = jobName})
-            end
+        if(lastjob ~= info['job']) then
+            lastjob = info['job']
+            SendNUIMessage({job = info['job']})
         end
 
         if(lastcash ~= info['money']) then
@@ -330,7 +310,6 @@ comma_value = function(amount)
     end
     return formatted
 end
-
 
 -- Speedometer and a few other things
 local checkvehclass = true
