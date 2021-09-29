7a89d57abe626b90389b2ab0f5f59951ff35460e
Fezz <43589668+FBFezz@users.noreply.github.com>
2021-09-30 00:02:30 +0100

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,6 @@
+Config = {}
+
+Config.rpRadio = false -- Enable if you are using rp-radio
+Config.LegacyFuel = false -- Enable if you are using LegacyFuel
+Config.SpeedFPS = 125
+Config.Speed = 3.6 -- 3.6 for kmh, 2.4 for MPH
