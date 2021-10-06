ca38c16ef14bc0f2d92a9398979582f0dce6f8a1
Fezz <43589668+FBFezz@users.noreply.github.com>
2021-10-06 22:21:05 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -3,4 +3,4 @@ Config = {}
 Config.rpRadio = false -- Enable if you are using rp-radio
 Config.LegacyFuel = false -- Enable if you are using LegacyFuel
 Config.SpeedFPS = 125
-Config.Speed = 3.6 -- 3.6 for kmh, 2.236936 for MPH
+Config.Speed = 2.236936 -- 3.6 for kmh, 2.236936 for MPH
